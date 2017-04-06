e47304915535d3d27738fcc097e382b13fc57363
João Conde <joaoconde_13@hotmail.com>
2017-04-06 16:26:18 +0100

    Beginning of the project.It runs and displays "hello world"

--- core/src/com/spacecruiser/game/Game.java
+++ core/src/com/spacecruiser/game/Game.java
@@ -5,29 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 public class Game extends ApplicationAdapter {
+
 	SpriteBatch batch;
-	Texture img;
+	Texture helloImg;
+
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		helloImg = new Texture(Gdx.files.internal("helloworld.jpg"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(helloImg,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		helloImg.dispose();
 	}
 }
