930a87680840ccbe9f7d7788c267cb708d3ca1a3
João Conde <joaoconde_13@hotmail.com>
2017-04-13 21:20:46 +0100

    created animation

--- core/src/com/spacecruiser/game/Game.java
+++ core/src/com/spacecruiser/game/Game.java
@@ -2,32 +2,67 @@ package com.spacecruiser.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Game extends ApplicationAdapter {
+
+	AssetManager manager;
 	SpriteBatch batch;
-	Texture img;
+
+    Texture pikachu;
+
+    Animation pikachuAttack;
+
+    private float elapsedTime;
+
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+        pikachu = new Texture("pikachuQuickAttack.jpg");
+        pikachuAttack = new Animation(.25f,createAnimationFrames(pikachu,2,4));
+
+		manager = new AssetManager();
+        manager.load("pikachuQuickAttack.jpg", Texture.class);
+		manager.finishLoading();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+
+        elapsedTime += Gdx.graphics.getDeltaTime();
+
+        Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+        batch.draw((TextureRegion) pikachuAttack.getKeyFrame(elapsedTime,true),0,0);
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		manager.dispose();
 	}
+
+
+	public TextureRegion[] createAnimationFrames(Texture animation, int col, int lin){
+
+        TextureRegion temp[][] = TextureRegion.split(animation,animation.getWidth() / col, animation.getHeight() / lin);
+        TextureRegion frames[] = new TextureRegion[col*lin];
+        System.arraycopy(temp[0],0,frames,0,2);
+
+        return frames;
+    }
 }
