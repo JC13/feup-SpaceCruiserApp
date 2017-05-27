a7b5b9d6cbdaaf2b75256a0cd559501426231b6b
Nelson Costa <nelsoncosta@outlook.com>
2017-05-27 18:15:21 +0100

    Not working, but has the structure of other project in which it works…

---
+++ core/src/com/spacecruiser/game/tests/SimpleTest.java
@@ -0,0 +1,21 @@
+package com.spacecruiser.game.tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+public class SimpleTest {
+
+    @Test
+    public void SimpleTest(){
+        assertEquals(2, is(2));
+    }
+}
